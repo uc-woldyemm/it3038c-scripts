826d5d1f9cb3246f481b74d097080b9431415a93
unknown <woldyemm@mail.uc.edu>
2021-01-31 23:43:56 -0500

    adding powershell files

---
+++ sysinfo.ps1
@@ -0,0 +1,11 @@
+﻿function gitIP {
+(Get-NetIPAddress) IPv4Address | Select-String "192"
+}
+write-host(getIP)
+$IP = getIP
+$Date = Get-Date
+$Body = "This machine's IP is SIP. User is env:username. Hostname is $. PowerShell Version. Today's date is $DATE"
+
+write-host($Body)
+
+#Send-MailMessage -To "woldyemm@mail.uc.edu" -From "mekdin28@gmail.com" -Subject "IT3038c windows sysinfo" -Body $Body -SmtpServer smtp.google.com -port 587 -UseSSL -Credential
