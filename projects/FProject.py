6d45585646a3b68a9778e66195e737b14c733619
uc-woldyemm <77596941+uc-woldyemm@users.noreply.github.com>
2021-04-20 22:26:14 -0400

    Update FProject.py

--- projects/FProject.py
+++ projects/FProject.py
@@ -1,8 +1,8 @@
 import random 
- 
+# Creates variables for random numbers 
 odd_number = random.randint(1,10)
 tries = 1
- 
+ # Asks for users input name & if they want to play the guessing game
 name = input("Hello, What's your Name cutiepie ")
  
 print("Hello", name+",", )
@@ -11,7 +11,7 @@ question = input("Would you Like To Guess a Number ? [Y/N] ")
  
 if question == "n":
     print("oh..no problem")
- 
+ # Creates main game loop
 if question == "y":
     print("Say A Number Between 1 & 10")
     guess = int(input("Make a Guess, I will be nice and give one hint;P : "))
@@ -19,7 +19,7 @@ if question == "y":
         print("Guess Lower than ", guess)
     if guess < odd_number:
         print("Guess Higher than ", guess)
- 
+   # Do while loop that continues to check for the correct answer
     while guess != odd_number:
         tries += 1
         guess = int(input("Try Again : ")) 
